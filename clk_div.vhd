3697fba5c0f1ef8da3af51b3e1e02706927c71bb
ERICK <50150389+erickdiias@users.noreply.github.com>
2025-01-07 20:29:06 -0300

    Update clk_div.vhd

--- clk_div.vhd
+++ clk_div.vhd
@@ -5,10 +5,11 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity clk_div is
     generic(
-        freq_clk : integer := 100e6
+        freq_clk : integer
     );
     port(
         clk : in std_logic;
@@ -23,7 +24,7 @@ begin
     process(clk)
     begin
         if rising_edge(clk) then
-            if contagem = freq_clk then
+            if contagem = freq_clk / 2 then
                 contagem <= 0;
                 clk_1Hz_int <= not clk_1Hz_int;
             else
